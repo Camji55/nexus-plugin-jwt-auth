9166fffa7ce9e87f09ba16e84f6403ce4544b81b
Cameron Ingham <camji555@gmail.com>
2020-04-25 10:44:08 -0700

    fix: enforced settings requirement

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { PluginEntrypoint } from 'nexus/plugin'
 import { Settings } from './settings'
 import { applyMiddleware } from './middleware'
 
-export const auth: PluginEntrypoint<Settings> = settings => {
+export const auth: PluginEntrypoint<Settings, 'required'> = settings => {
   const protectedPaths = settings?.protectedPaths
   if (protectedPaths) {
     applyMiddleware(protectedPaths)
