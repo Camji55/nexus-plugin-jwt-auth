db314d537b00f5534ff42d4da5da916eae7d4d86
John WALTER <john.walter@wassa.fr>
2020-07-28 22:46:52 +0200

    fix: cookie settings should not be mandatory

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,9 @@
-import { Request } from 'express';
+import { Request } from "express";
 
 export type Settings = {
-    appSecret: string
-    protectedPaths?: string[],
-    useCookie: boolean
-    cookieName: string
-    verify?: (req: Request) => Promise<any> | any
-}
+  appSecret: string;
+  protectedPaths?: string[];
+  useCookie?: boolean;
+  cookieName?: string;
+  verify?: (req: Request) => Promise<any> | any;
+};
